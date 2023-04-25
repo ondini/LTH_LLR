442814c43cf768210fdebbd1d78cd99ee4e9d03c
Ondin <kafka.ondrej@email.cz>
2023-04-25 15:35:09 +0200

    Solution to part 3 of project 1 added.

--- project1/R/part3.R
+++ project1/R/part3.R
@@ -88,30 +88,110 @@ model.pred$r <- rstudent(model)
 
 
 ### Plot r* vs yhat####
-ggplot(model.pred, aes(x = fit, y = r)) +
-  geom_point(size = 3) +
-  geom_hline(yintercept = 0) +
-  geom_hline(yintercept = c(-2, 2)) +
-  geom_hline(yintercept = c(-3, 3), linetype = "dashed") +
-  geom_smooth() +
-  xlab("Fitted values") +
-  ylab("r*") +
-  labs(title = "Studentized residuals vs fitted values") +
-  labs(caption = "y = +/- 2 and +/- 3") +
-  theme(text = element_text(size = 18))
+(
+  plot.rdata <- 
+    ggplot(model.pred, aes(x = fit, y = r)) +
+    geom_point(size = 3) +
+    geom_hline(yintercept = 0) +
+    geom_hline(yintercept = c(-2, 2)) +
+    geom_hline(yintercept = c(-3, 3), linetype = "dashed") +
+    geom_smooth() +
+    xlab("Fitted values") +
+    ylab("r*") +
+    labs(title = "Studentized residuals vs fitted values") +
+    labs(caption = "y = +/- 2 and +/- 3") +
+    theme(text = element_text(size = 18))
+)
+
+ggsave(filename = "project1/plots/rdata.png", plot = plot.rdata)
 
 ##Plot sqrt(|r*|) vs yhat####
-ggplot(model.pred, aes(x = fit, y = sqrt(abs(r)))) +
-  geom_point(size = 3) +
-  geom_hline(yintercept = c(sqrt(qnorm(0.75)), sqrt(2))) +
-  geom_hline(yintercept = sqrt(3), linetype = 2) +
-  geom_smooth() +
-  xlab("Fitted values") +
-  ylab("sqrt(|r*|)") +
-  labs(title = "Studentized residuals vs fitted values") +
-  labs(caption = "y = 0.82, sqrt(2), sqrt(3)") +
-  theme(text = element_text(size = 18))
+(
+  plot.rsdata <- 
+    ggplot(model.pred, aes(x = fit, y = sqrt(abs(r)))) +
+    geom_point(size = 3) +
+    geom_hline(yintercept = c(sqrt(qnorm(0.75)), sqrt(2))) +
+    geom_hline(yintercept = sqrt(3), linetype = 2) +
+    geom_smooth() +
+    xlab("Fitted values") +
+    ylab("sqrt(|r*|)") +
+    labs(title = "Studentized residuals vs fitted values") +
+    labs(caption = "y = 0.82, sqrt(2), sqrt(3)") +
+    theme(text = element_text(size = 18))
+)
+
+ggsave(filename = "project1/plots/rsdata.png", plot = plot.rsdata)
+
+# 3.C ####
+
+## Add seasonal variable to dataframe ####
+weather$monthnr <- as.factor(substr(weather$month, 6, 7))
+
+## Fit the full and null models ####
+
+fullmod <- lm(rain^(1/3) ~ pressure*location*speed*temp*monthnr, data = weather)
+
+nullmod <- lm(rain^(1/3) ~ 1, data = weather)
+
+## Choose best model using BIC and Stepwise selection ####
+bicModel <- step(nullmod, scope = list(lower=nullmod,upper=fullmod),
+     direction="both", criterion = "BIC",  k = log(nrow(weather)))
+
+## Compare all models
+model = bicModel # select model and run the commands below
+
+model.sum <- summary(model)
+length(model$coefficients)
+model.sum$r.squared
+model.sum$adj.r.squared
+AIC(model)
+BIC(model)
+
+## Plot BIC model residuals ####
+
+model = bicModel
+model.pred <- 
+  cbind(weather,
+        fit = predict(model),
+        conf = predict(model, interval = "confidence"),
+        pred = predict(model, interval = "prediction"))
 
 
+## Compute studentized residuals ####
+model.pred$r <- rstudent(model)
 
+### Plot r* vs yhat####
+(
+  plot.rdatabic <- 
+    ggplot(model.pred, aes(x = fit, y = r)) +
+    geom_point(size = 3) +
+    geom_hline(yintercept = 0) +
+    geom_hline(yintercept = c(-2, 2)) +
+    geom_hline(yintercept = c(-3, 3), linetype = "dashed") +
+    geom_smooth() +
+    xlab("Fitted values") +
+    ylab("r*") +
+    labs(title = "Studentized residuals vs fitted values") +
+    labs(caption = "y = +/- 2 and +/- 3") +
+    theme(text = element_text(size = 18))
+)
+
+ggsave(filename = "project1/plots/rdatabic.png", plot = plot.rdatabic)
+
+##Plot sqrt(|r*|) vs yhat####
+(
+  plot.rsdatabic <- 
+    ggplot(model.pred, aes(x = fit, y = sqrt(abs(r)))) +
+    geom_point(size = 3) +
+    geom_hline(yintercept = c(sqrt(qnorm(0.75)), sqrt(2))) +
+    geom_hline(yintercept = sqrt(3), linetype = 2) +
+    geom_smooth() +
+    xlab("Fitted values") +
+    ylab("sqrt(|r*|)") +
+    labs(title = "Studentized residuals vs fitted values") +
+    labs(caption = "y = 0.82, sqrt(2), sqrt(3)") +
+    theme(text = element_text(size = 18))
+)
+
+ggsave(filename = "project1/plots/rsdata.png", plot = plot.rsdatabic)
 
